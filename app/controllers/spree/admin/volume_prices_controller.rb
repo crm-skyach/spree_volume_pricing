c47ac3ce73afb5bb4a0f8362f0617057152f85e0
dinesh-skyach <dinesh.chohda@skyach.com>
2021-08-05 11:31:29 +0530

    Changes in the custom branch

--- app/controllers/spree/admin/volume_prices_controller.rb
+++ app/controllers/spree/admin/volume_prices_controller.rb
@@ -3,8 +3,12 @@ module Spree
     class VolumePricesController < Spree::Admin::BaseController
       def destroy
         @volume_price = Spree::VolumePrice.find(params[:id])
-        @volume_price.destroy
-        render nothing: true
+        if @volume_price.destroy
+          respond_to do |format|
+            format.js {render inline: "location.reload();" }
+            flash[:error] = 'Volume price was successfully destroyed.'
+          end
+        end
       end
     end
   end
